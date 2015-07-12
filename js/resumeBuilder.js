fed66362f74812fb5f00dca555e04ccf6ad43f8d
hshin987 <graceshin@hotmail.com>
2015-07-11 20:38:55 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,3 @@
 $("#main").append("Hyewon Shin");
 
-var formattedName = HTMLheaderName.replace(
-  "%data%", Hyewon Shin);
-var role = "Web Developer"
-var formattedRole = HTMLheaderRole.replace(
-  "%data%", role);
-  
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+
